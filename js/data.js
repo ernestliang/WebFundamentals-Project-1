a75379c517f9ca0ff13539b21b0ec052eb346330
ernestliang <103881112+ernestliang@users.noreply.github.com>
2022-06-12 20:59:29 +0800

    Cat Beginning

--- js/data.js
+++ js/data.js
@@ -1,3 +1,17 @@
-const arrayCategories = [
-    
+const CategoriesArray = [
+    {categName: "Electronics", categID: 0},
+    {categName: "Tools", categID: 1},
+    {categName: "Accesories", categID: 2}
 ];
+
+const ProductsArray = [
+    {productName: "Sunsang Note 3D0", productID: 0},
+    {productName: "Ranger VI Power Laptop v12", productID: 0},
+    {productName: "", productID: 0},
+    {productName: "Bronx All-Purpose Swiss Army Knife", productID: 1},
+    {productName: "Legion x1000 Power Drill", productID: 1},
+    {productName: "", productID: 1},
+    {productName: "", productID: 2},
+    {productName: "", productID: 2},
+    {productName: "", productID: 2}
+]
