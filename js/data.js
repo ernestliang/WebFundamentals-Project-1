d540271c97eefc8a03e7f6ecc85dd6b1ee9069e8
ernestliang <103881112+ernestliang@users.noreply.github.com>
2022-06-12 21:54:39 +0800

    Categ Start

--- js/data.js
+++ js/data.js
@@ -1,17 +1,18 @@
 const CategoriesArray = [
-    {categName: "Electronics", categID: 0},
-    {categName: "Tools", categID: 1},
-    {categName: "Accesories", categID: 2}
+    {ID: 0, categName: "Electronics"},
+    {ID: 1, categName: "Tools"},
+    {ID: 2, categName: "Accesories"},
+    {ID: 3, categName: "Bags"}
 ];
 
 const ProductsArray = [
-    {productName: "Sunsang Note 3D0", productID: 0},
-    {productName: "Ranger VI Power Laptop v12", productID: 0},
-    {productName: "", productID: 0},
-    {productName: "Bronx All-Purpose Swiss Army Knife", productID: 1},
-    {productName: "Legion x1000 Power Drill", productID: 1},
-    {productName: "", productID: 1},
-    {productName: "", productID: 2},
-    {productName: "", productID: 2},
-    {productName: "", productID: 2}
+    {ID: 0, productName: "Sunsang Note 3D0",                    categID: 0, img:"", price:0, description:"", review:""},
+    {ID: 1, productName: "Ranger VI Power Laptop v12",          categID: 0, img:"", price:0, description:"", review:""},
+    {ID: 2, productName: "",                                    categID: 1, img:"", price:0, description:"", review:""},
+    {ID: 3, productName: "Bronx All-Purpose Swiss Army Knife",  categID: 1, img:"", price:0, description:"", review:""},
+    {ID: 4, productName: "Legion x1000 Power Drill",            categID: 2, img:"", price:0, description:"", review:""},
+    {ID: 5, productName: "",                                    categID: 2, img:"", price:0, description:"", review:""},
+    {ID: 6, productName: "",                                    categID: 2, img:"", price:0, description:"", review:""},
+    {ID: 7, productName: "",                                    categID: 3, img:"", price:0, description:"", review:""},
+    {ID: 8, productName: "",                                    categID: 3, img:"", price:0, description:"", review:""}
 ]
