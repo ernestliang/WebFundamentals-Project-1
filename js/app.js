bf34c50e605c6b059c665265209f9f53e045de8f
ernestliang <103881112+ernestliang@users.noreply.github.com>
2022-06-15 20:55:46 +0800

    href

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 function getPrdFrmCat(category) {
-    var getImg = ProductsArray.filter (x => x.categID == category);
+    var products = ProductsArray.filter (x => x.categID == category);
 
     debugger;
     var img1 = document.getElementById("prdImg1");
     var img2 = document.getElementById("prdImg2");
     
 
-    img1.src = getImg[0].img;
-    img2.src = getImg[1].img;
+    img1.src = products[0].img;
+    img2.src = products[1].img;
+
+    var ele1 = document.getElementById("href1");
+    var ele2 = document.getElementById("href2");
+
+    ele1.href = 'ProductPage.html?id=' + products[0].ID;
+    ele2.href = 'ProductPage.html?id=' + products[1].ID;
 }
