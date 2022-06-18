79f0ce7306ebe416ecdaff2e35fef8b9220b0a9f
ernestliang <103881112+ernestliang@users.noreply.github.com>
2022-06-18 19:52:40 +0800

    Changes

--- js/app.js
+++ js/app.js
@@ -16,6 +16,42 @@ function getPrdFrmCat(category) {
     ele2.href = 'ProductPage.html?id=' + products[1].ID;
 }
 
-function changeHREF() {
-    
+function getNewArrivalProducts() {
+
+    var newProducts = ProductsArray.filter(x => x.type == 1);
+    // Solving the Href's
+    document.getElementById("newHref1").href = "ProductPage.html?id=" + newProducts[0].ID;
+    document.getElementById("newHref2").href = "ProductPage.html?id=" + newProducts[1].ID;
+    document.getElementById("newHref3").href = "ProductPage.html?id=" + newProducts[2].ID;
+
+    // Solving the Prices
+    document.getElementById("newImg1").src = newProducts[0].img;
+    document.getElementById("newImg2").src = newProducts[1].img;
+    document.getElementById("newImg3").src = newProducts[2].img;
+
+    // Solving the Image Source
+    document.getElementById("newPrice1").innerHTML = "Now going for " + newProducts[0].price + "$";
+    document.getElementById("newPrice2").innerHTML = "Now going for " + newProducts[1].price + "$";
+    document.getElementById("newPrice3").innerHTML = "Now going for " + newProducts[2].price + "$";
+}
+
+function getDiscountProducts() {
+
+    var discProducts = ProductsArray.filter(x => x.type == 2);
+
+    debugger;
+    // Solving the Href's
+    document.getElementById("discHref1").href = "ProductPage.html?id=" + discProducts[0].ID;
+    // document.getElementById("discHref2").href = "ProductPage.html?id=" + discProducts[1].ID;
+    // document.getElementById("discHref3").href = "ProductPage.html?id=" + discProducts[2].ID;
+
+    // Solving the Prices
+    document.getElementById("discImg1").src = discProducts[0].img;
+    document.getElementById("discImg2").src = discProducts[1].img;
+    document.getElementById("discImg3").src = discProducts[2].img;
+
+    // Solving the Image Source
+    document.getElementById("discPrice1").innerHTML = "Now going for " + discProducts[0].price + "$";
+    document.getElementById("discPrice2").innerHTML = "Now going for " + discProducts[1].price + "$";
+    document.getElementById("discPrice3").innerHTML = "Now going for " + discProducts[2].price + "$";
 }
--- js/data.js
+++ js/data.js
@@ -5,23 +5,24 @@ const CategoriesArray = [
     {ID: 3, categName: "Bags"}
 ];
 
-const ProductsArray = [
-    {ID: 0, productName: "Sunsang Note 3D0",                    categID: 0, img:"images/CategoryProducts/Smartphone.webp",          price:0, description:"Desc1", review:"", type: 0},
-    {ID: 1, productName: "Ranger VI Power Laptop v12",          categID: 0, img:"images/CategoryProducts/Laptop.webp",              price:0, description:"Desc2", review:"", type: 0},
-    {ID: 2, productName: "Legion x1000 Power Drill",            categID: 1, img:"images/CategoryProducts/PowerDrill.jpg",           price:0, description:"Desc3", review:"", type: 0},
-    {ID: 3, productName: "Bronx All-Purpose Swiss Army Knife",  categID: 1, img:"images/CategoryProducts/SwissArmyKnife.webp",      price:0, description:"Desc4", review:"", type: 0},
-    {ID: 4, productName: "Starcraft Dominion Keychain",         categID: 2, img:"images/CategoryProducts/Keychain.webp",            price:0, description:"Desc5", review:"", type: 0},
-    {ID: 5, productName: "Dark Souls Ring Collection",          categID: 2, img:"images/CategoryProducts/RingCollection.jpg",       price:0, description:"Desc5", review:"", type: 0},
-    {ID: 7, productName: "Napkan Survival Knapsack",            categID: 3, img:"images/CategoryProducts/SurvivalBackpack.webp",    price:0, description:"Desc7", review:"", type: 0},
-    {ID: 8, productName: "ChopItAll Machete Deluxe",            categID: 3, img:"images/CategoryProducts/Machete.jpg",              price:0, description:"Desc8", review:"", type: 0},
+// This is going to be the Descriptions
 
+const ProductsArray = [
+    {ID: 0, productName: "Sunsang Note 3D0",                    categID: 0, img:"images/CategoryProducts/Smartphone.webp",          price:0, description:"Desc1",  type: 0, review:""},
+    {ID: 1, productName: "Ranger VI Power Laptop v12",          categID: 0, img:"images/CategoryProducts/Laptop.webp",              price:0, description:"Desc2",  type: 0, review:""},
+    {ID: 2, productName: "Legion x1000 Power Drill",            categID: 1, img:"images/CategoryProducts/PowerDrill.jpg",           price:0, description:"Desc3",  type: 0, review:""},
+    {ID: 3, productName: "Bronx All-Purpose Swiss Army Knife",  categID: 1, img:"images/CategoryProducts/SwissArmyKnife.webp",      price:0, description:"Desc4",  type: 0, review:""},
+    {ID: 4, productName: "Starcraft Dominion Keychain",         categID: 2, img:"images/CategoryProducts/Keychain.webp",            price:0, description:"Desc5",  type: 0, review:""},
+    {ID: 5, productName: "Dark Souls Ring Collection",          categID: 2, img:"images/CategoryProducts/RingCollection.jpg",       price:0, description:"Desc5",  type: 0, review:""},
+    {ID: 6, productName: "Napkan Survival Knapsack",            categID: 3, img:"images/CategoryProducts/SurvivalBackpack.webp",    price:0, description:"Desc7",  type: 0, review:""},
+    {ID: 7, productName: "ChopItAll Machete Deluxe",            categID: 3, img:"images/CategoryProducts/Machete.jpg",              price:0, description:"Desc8",  type: 0, review:""},
     // This is for the New Arrivals
-    {ID: 9, productName: "Asus® Archon Laptop v8.2",            categID: 4, img:"images/New Arrivals/Laptop.png",                   price:0, description:"Desc9", review:"", type: 1},
-    {ID: 10, productName: "Nydex® Heavy Duty Travel Backpack",   categID: 4, img:"images/New Arrivals/Backpack.jpg",                 price:0, description:"Desc10", review:"", type: 1},
-    {ID: 11, productName: "Fixd® All Purpose Toolbox",           categID: 4, img:"images/New Arrivals/Toolbox.jpg",                  price:0, description:"Desc11", review:"", type: 1},
+    {ID: 8, productName: "Asus® Archon Laptop v8.2",            categID: 4, img:"images/New Arrivals/Laptop.png",                   price:0, description:"Desc9",  type: 1, review:""},
+    {ID: 9, productName: "Nydex® Heavy Duty Travel Backpack",   categID: 4, img:"images/New Arrivals/Backpack.jpg",                 price:0, description:"Desc10", type: 1, review:""},
+    {ID: 10, productName: "Fixd® All Purpose Toolbox",           categID: 4, img:"images/New Arrivals/Toolbox.jpg",                 price:0, description:"Desc11", type: 1, review:""},
 
     // This is for the Discounts
-    {ID: 12, productName: "Grandiose Perak Keris Letter Opener", categID: 5, img:"images/Discounts/LetterOpener.jpg",                price:0, description:"Desc12", review:"", type: 2},
-    {ID: 13, productName: "FlexSaw Heavy Duty Hacksaw",          categID: 5, img:"images/Discounts/Saw.jpg",                         price:0, description:"Desc13", review:"", type: 2},
-    {ID: 14, productName: "Succital Quiet Vacuum Cleaner",       categID: 5, img:"images/Discounts/Vacuum.jpg",                      price:0, description:"Desc14", review:"", type: 2}
+    {ID: 11, productName: "Grandiose Perak Keris Letter Opener", categID: 5, img:"images/Discounts/LetterOpener.jpg",               price:0, description:"Desc12", type: 2, review:""},
+    {ID: 12, productName: "FlexSaw Heavy Duty Hacksaw",          categID: 5, img:"images/Discounts/Saw.jpg",                        price:0, description:"Desc13", type: 2, review:""},
+    {ID: 13, productName: "Succital Quiet Vacuum Cleaner",       categID: 5, img:"images/Discounts/Vacuum.jpg",                     price:0, description:"Desc14", type: 2, review:""}
 ]
